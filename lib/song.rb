a303e20fc5528c934190d462695cf25bc9a73a9f
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-11-18 19:39:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,8 @@ class Song
   def self.new_by_filename(name)
     song_info = name.split(" - ")
     song = self.new(song_info[1])
-    song.artist = song_info[0]
+    song.artist = Artist.new(song_info[0])
+    song
   end 
   
 end 
